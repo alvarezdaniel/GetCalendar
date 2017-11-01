af76fd0d00f62c20bcbaad6cb288d7df0d9d77ae
Daniel Alvarez <alvarezd@kapsch.net>
2017-11-01 11:56:07 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+//using Microsoft.Office.Interop.Outlook;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace GetCalendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetOutlookCalendar("Alvarez Daniel Calendar.ics");
+        }
+
+        // https://msdn.microsoft.com/en-us/library/office/bb647583.aspx
+        private static void GetOutlookCalendar(string calendarFileName)
+        {
+            if (string.IsNullOrEmpty(calendarFileName))
+                throw new ArgumentException("calendarFileName",
+                "Parameter must contain a value.");
+
+            var app = new Outlook.Application();
+
+            Outlook.Folder calendar = app.Session.GetDefaultFolder(
+                Outlook.OlDefaultFolders.olFolderCalendar) as Outlook.Folder;
+            Outlook.CalendarSharing exporter = calendar.GetCalendarExporter();
+
+            // Set the properties for the export
+            exporter.CalendarDetail = Outlook.OlCalendarDetail.olFullDetails;
+            exporter.IncludeAttachments = true;
+            exporter.IncludePrivateDetails = true;
+            exporter.RestrictToWorkingHours = false;
+            exporter.IncludeWholeCalendar = true;
+
+            // Save the calendar to disk
+            exporter.SaveAsICal(Path.Combine(Environment.CurrentDirectory, calendarFileName));
+        }
+    }
+}
